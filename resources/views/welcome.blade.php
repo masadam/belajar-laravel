6fe1eb0031bf854d40cffcfa53e0029b9eb50957
NPC_7 <431735+masadam@users.noreply.github.com>
2019-10-15 13:42:57 +0700

    commit kedua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rohman Jahad
                 </div>
 
                 <div class="links">
